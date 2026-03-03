12afac0879d2b50aad2c38b1f91d41bb29243189
Yogeesh N C <yogeeshnc832@gmail.com>
2026-03-03 21:30:50 +0530

    Time: 13 ms (87.44%), Space: 87.6 MB (70.19%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>(nums.length);
+        for (int num : nums) {
+            if (!seen.add(num)) {
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+}
